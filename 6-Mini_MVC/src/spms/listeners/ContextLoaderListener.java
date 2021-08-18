3e3c46145397ee3b0e2bae37a795c9073c3e0645
leo66 <leo66@DESKTOP-RLELS97>
2021-08-18 14:24:11 +0900

    < Step 5: ~ 6.5 > Using application-context.properties

--- 6-Mini_MVC/src/spms/listeners/ContextLoaderListener.java
+++ 6-Mini_MVC/src/spms/listeners/ContextLoaderListener.java
@@ -1,39 +1,25 @@
 package spms.listeners;
 
-import javax.naming.InitialContext;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.sql.DataSource;
 
-import spms.controls.LogInController;
-import spms.controls.LogOutController;
-import spms.controls.MemberAddController;
-import spms.controls.MemberDeleteController;
-import spms.controls.MemberListController;
-import spms.controls.MemberUpdateController;
-import spms.dao.MemberDao;
-import spms.dao.MySqlMemberDao;
+import spms.context.ApplicationContext;
 
 public class ContextLoaderListener implements ServletContextListener {
+  static ApplicationContext applicationContext;
+  
+  public static ApplicationContext getApplicationContext() {
+    return applicationContext;
+  }
   
   @Override
   public void contextInitialized(ServletContextEvent event) {
     try {
       ServletContext sc = event.getServletContext();
       
-      InitialContext initialContext = new InitialContext();
-      DataSource ds = (DataSource)initialContext.lookup("java:comp/env/jdbc/studydb");
-      
-      MySqlMemberDao memberDao = new MySqlMemberDao();
-      memberDao.setDataSource(ds);
-            
-      sc.setAttribute("/auth/login.do", new LogInController().setMemberDao(memberDao));
-      sc.setAttribute("/auth/logout.do", new LogOutController());
-      sc.setAttribute("/member/list.do", new MemberListController().setMemberDao(memberDao));
-      sc.setAttribute("/member/add.do", new MemberAddController().setMemberDao(memberDao));
-      sc.setAttribute("/member/update.do", new MemberUpdateController().setMemberDao(memberDao));
-      sc.setAttribute("/member/delete.do", new MemberDeleteController().setMemberDao(memberDao));
+      String propertiesPath = sc.getRealPath(sc.getInitParameter("contextConfigLocation"));
+      applicationContext = new ApplicationContext(propertiesPath);
       
     } catch(Throwable e) {
       e.printStackTrace();
--- 6-Mini_MVC/src/spms/servlets/DispatcherServlet.java
+++ 6-Mini_MVC/src/spms/servlets/DispatcherServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.HashMap;
 
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +12,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import spms.bind.DataBinding;
 import spms.bind.ServletRequestDataBinder;
+import spms.context.ApplicationContext;
 import spms.controls.Controller;
+import spms.listeners.ContextLoaderListener;
 
 @SuppressWarnings("serial")
 @WebServlet("*.do")
@@ -24,11 +25,15 @@ public class DispatcherServlet extends HttpServlet {
     String servletPath = request.getServletPath();
     
     try {
-      ServletContext sc = this.getServletContext();
+      ApplicationContext ctx = ContextLoaderListener.getApplicationContext();
+      
       HashMap<String, Object> model = new HashMap<String, Object>();
       model.put("session", request.getSession());
       
-      Controller pageController = (Controller)sc.getAttribute(servletPath);
+      Controller pageController = (Controller)ctx.getBean(servletPath);
+      if(pageController == null) {
+        throw new Exception("요청한 서비스를 찾을 수 없습니다.");
+      }
       
       if(pageController instanceof DataBinding) {
         prepareRequestData(request, model, (DataBinding)pageController);
